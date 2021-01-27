23a9099021523733066f77a82a85d20453bf296d
abhijitpurru2 <68308831+abhijitpurru2@users.noreply.github.com>
2021-01-27 18:00:28 -0500

    Add files via upload

---
+++ Leaflet-Step-1/static/js/logic.js
@@ -0,0 +1,15 @@
+
+// Gets data from USGS
+var geoJson = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+
+// Creates map
+var myMap = L.map("map", {
+    center: [37.09, -95.71],
+    zoom: 5
+});
+
+
+function mapSelector(){
+
+}
