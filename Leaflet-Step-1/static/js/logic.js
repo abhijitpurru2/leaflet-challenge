51170974f602a0545b8c8607bd0cf2cbbf1899c9
abhijitpurru2 <68308831+abhijitpurru2@users.noreply.github.com>
2021-01-27 19:06:02 -0500

    Add files via upload

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -11,6 +11,7 @@ var myMap = L.map("map", {
 function createMap(){
     L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
       attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+      tileSize: 512,
       maxZoom: 18,
       zoomOffset: -1,
       id: "mapbox/streets-v11",
@@ -48,7 +49,7 @@ d3.json(geoURL, function (geoData) {
 
     for (var j = 0; j < info.length; j++){
         console.log(info[j]);
-        var coordinates = [info[j].geometry.coordinates[0], info[j].geometry.coordinates[1]];
+        var coordinates = [info[j].geometry.coordinates[1], info[j].geometry.coordinates[0]];
         L.circle(coordinates, {
                 fillOpacity: 0.75,
                 color: "black",
